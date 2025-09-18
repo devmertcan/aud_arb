b019c58e40780c9d6ebb1992718f7c649a071228
devmertcan <mertcanates210@gmail.com>
2025-09-17 17:18:27 -0700

    Check websocket connector

--- src/main.py
+++ src/main.py
@@ -3,10 +3,7 @@ from connectors.ws_connectors import subscribe_ir_orderbook
 
 async def main():
     async def print_ob(data):
-        # For now this will just print RAW messages
-        pass
-
+        print("Orderbook update:", data)
     await subscribe_ir_orderbook("BTC/AUD", print_ob)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
