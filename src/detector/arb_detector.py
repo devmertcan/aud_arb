5f6ff9e81aa882645c09d5fb3f6890b34cfd2d04
devmertcan <mertcanates210@gmail.com>
2025-09-18 22:42:42 -0700

    update checklist

--- src/detector/arb_detector.py
+++ src/detector/arb_detector.py
@@ -7,17 +7,19 @@ class ArbitrageDetector:
         ex_names = list(self.orderbooks.keys())
         for i in range(len(ex_names)):
             for j in range(i + 1, len(ex_names)):
-                ex_a = ex_names[i]
-                ex_b = ex_names[j]
-                ob_a = self.orderbooks[ex_a]
-                ob_b = self.orderbooks[ex_b]
+                ex_a, ex_b = ex_names[i], ex_names[j]
+                ob_a, ob_b = self.orderbooks[ex_a], self.orderbooks[ex_b]
 
-                bid_a = ob_a.best_bid()
-                ask_b = ob_b.best_ask()
+                bid_a, ask_a = ob_a.best_bid(), ob_a.best_ask()
+                bid_b, ask_b = ob_b.best_bid(), ob_b.best_ask()
+
                 if bid_a and ask_b:
                     spread = bid_a[0] - ask_b[0]
                     if spread > ask_b[0] * self.threshold:
-                        print(
-                            f"[OPPORTUNITY] Buy {ex_b} @ {ask_b[0]} / "
-                            f"Sell {ex_a} @ {bid_a[0]} | Spread {spread:.2f}"
-                        )
+                        self.report(f"[OPPORTUNITY] Buy {ex_b} @ {ask_b[0]} / Sell {ex_a} @ {bid_a[0]} | Spread {spread:.2f}")
+
+                if bid_b and ask_a:
+                    spread = bid_b[0] - ask_a[0]
+                    if spread > ask_a[0] * self.threshold:
+                        self.report(f"[OPPORTUNITY] Buy {ex_a} @ {ask_a[0]} / Sell {ex_b} @ {bid_b[0]} | Spread {spread:.2f}")
+
